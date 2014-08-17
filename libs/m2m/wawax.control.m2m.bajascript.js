d0f46ea4addc4185b6331ed8a4c3a0264291f950
antowan <antoine.sauvage@wawax.co>
2014-08-17 15:21:40 +0900

    update interface with japanese translation

--- libs/m2m/wawax.control.m2m.bajascript.js
+++ libs/m2m/wawax.control.m2m.bajascript.js
@@ -15,93 +15,93 @@
  */
 jQuery(function($) {
 
-	/**
-	 * Read datapoint value from Niagara station
-	 * 
-	 * @param {Object}
-	 *            path : ord
-	 * @param {Object}
-	 *            option : facet //TODO
-	 */
-	$.fn.controlBajaReadDataPoint = function(path, option) {
-		var control = this;
-		var sub = new baja.Subscriber();
-		var update = function() {
-			var value = null;
-			if (option == 1) {//full string display
-				value = this.getOutDisplay();
-				control.trigger('wxReadDataPointUpdate', value.toFixed(1) + '');
-			} else {//just the value
-				value = this.getOut().getValue();
-				control.trigger('wxReadDataPointUpdate', value.toFixed(1) + '');
-			}
-		};
-		sub.attach("changed", update);
-		baja.Ord.make(path).get({
-			subscriber : sub
-		});
-	};
-	
-	/**
-	 * Write datapoint on the station
-	 * 
-	 * @param {Object}
-	 *            path : ord
-	 * @param {Object}
-	 *            myValue : numeric value //TODO precise type
-	 * @param {Object}
-	 *            options : null //TODO {type, priority, ...}
-	 */
-	$.fn.controlBajaWriteDataPoint = function(path, myValue, options) {
-		var control = this;
-		var writeVal = myValue;
-		baja.Ord.make(path).get({
-			ok : function(point) {
-				point.setFallback(baja.$("baja:StatusNumeric", {
-					value : writeVal
-				}));
-				control.trigger('wxWriteDataPointUpdate', writeVal);
-			}
-		});
-	};
-	
-	/**
-	 * Read Niagara historian
-	 * 
-	 * @param {Object}
-	 *            req : ord to an historian
-	 * @param {Object}
-	 *            option : null //TODO
-	 */
-	$.fn.controlBajaHistory = function(req, option) {
-		var control = this;
-		var myTable = new Array();
-		var i = 0;
+ /**
+  * Read datapoint value from Niagara station
+  * 
+  * @param {Object}
+  *            path : ord
+  * @param {Object}
+  *            option : facet //TODO
+  */
+ $.fn.controlBajaReadDataPoint = function(path, option) {
+  var control = this;
+  var sub = new baja.Subscriber();
+  var update = function() {
+   var value = null;
+   if (option == 1) {//full string display
+    value = this.getOutDisplay();
+    control.trigger('wxReadDataPointUpdate', value.toFixed(1) + '');
+   } else {//just the value
+    value = this.getOut().getValue();
+    control.trigger('wxReadDataPointUpdate', value.toFixed(1) + '');
+   }
+  };
+  sub.attach("changed", update);
+  baja.Ord.make(path).get({
+   subscriber : sub
+  });
+ };
+ 
+ /**
+  * Write datapoint on the station
+  * 
+  * @param {Object}
+  *            path : ord
+  * @param {Object}
+  *            myValue : numeric value //TODO precise type
+  * @param {Object}
+  *            options : null //TODO {type, priority, ...}
+  */
+ $.fn.controlBajaWriteDataPoint = function(path, myValue, options) {
+  var control = this;
+  var writeVal = myValue;
+  baja.Ord.make(path).get({
+   ok : function(point) {
+    point.setFallback(baja.$("baja:StatusNumeric", {
+     value : writeVal
+    }));
+    control.trigger('wxWriteDataPointUpdate', writeVal);
+   }
+  });
+ };
+ 
+ /**
+  * Read Niagara historian
+  * 
+  * @param {Object}
+  *            req : ord to an historian
+  * @param {Object}
+  *            option : null //TODO
+  */
+ $.fn.controlBajaHistory = function(req, option) {
+  var control = this;
+  var myTable = new Array();
+  var i = 0;
 
-		baja.Ord.make(req).get(
-		{
-			ok : function(result) {
-				baja.iterate(result.getColumns(), function(c) {
-				});
-			},
-			cursor : {
-				before : function() {
-				},
-				after : function() {
-					control.trigger('wxHistoDataSetLoaded', myTable);
-					console.log(myTable);
-				},
-				each : function() {
-					myTable[i] = new Object({
-						timestamp : this.get("timestamp").getJsDate()
-								.getTime(),
-						value : this.get("value")
-					});
-					i++;
-				},
-				limit : option,
-				offset : 0
-			}
-		});
-	};
+  baja.Ord.make(req).get(
+  {
+   ok : function(result) {
+    baja.iterate(result.getColumns(), function(c) {
+    });
+   },
+   cursor : {
+    before : function() {
+    },
+    after : function() {
+     control.trigger('wxHistoDataSetLoaded', myTable);
+     //console.log(myTable);
+    },
+    each : function() {
+     myTable[i] = new Object({
+      timestamp : this.get("timestamp").getJsDate()
+        .getTime(),
+      value : this.get("value")
+     });
+     i++;
+    },
+    limit : option,
+    offset : 0
+   }
+  });
+ };
 });
